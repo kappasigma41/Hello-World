5fd39987ccac052d9c8a98503f631ca84aa2b224
kappasigma41 <kappa.sigma.41@gmail.com>
2011-08-17 08:48:10 +0900

    add imWorried()

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
+	public static void imWorried(){
+		System.out.println("I'm worried this statement will be deleted".);
+	}
 }
 
