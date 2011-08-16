044679034218476a356c3dcac0f68025f040505d
kappasigma41 <kappa.sigma.41@gmail.com>
2011-08-17 08:53:15 +0900

    changed message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello, World!");
+		System.out.println("Hello, String World!");
 	}
 	public static void imWorried(){
 		System.out.println("I'm worried this statement will be deleted".);
